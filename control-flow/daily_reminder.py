f1bf992ee61af9b409bea9e460d7d919f39b5815
25janet <wanjirujay25@gmail.com>
2025-05-27 12:30:22 +0300

    daily remainder

---
+++ control-flow/daily_reminder.py
@@ -0,0 +1,22 @@
+task = input("Enter your task: ")
+priority = input("Priority (high/medium/low): ")
+time_bound = input("Is it time-bound? (yes/no):")
+match priority:
+
+    case "high":
+            if time_bound == "yes":
+                print(f"Remainder: '{task}' is a high priority task that requires immediate action today!")
+            else:
+                print(f"Note: '{task}' is a high priority task.Consider completing it when you have free time.")
+    case "medium":
+        if time_bound == "yes":
+            print(f"Remainder: '{task}' is a medium priority task that requires immediate action today!")
+        else:
+            print(f"Note: '{task}' is a medium priority task.Consider completing it when you have free time.")
+    case "low":
+        if time_bound == "yes":
+            print(f"Remainder: '{task}' is a low priority task that requires immediate action today!")
+        else:
+            print(f"Note: '{task}' is a low priority task.Consider completing it when you have free time.")
+    case _:
+        print("Invalid priority level entered. Please enter high, medium, or low.")
